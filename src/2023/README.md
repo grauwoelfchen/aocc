a0fd01ac8ce3c2c239b4f279a7274184fe5381b4
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-12-11 03:54:10 +0900

    Update day1 for the part two

---
+++ src/2023/README.md
@@ -0,0 +1,7 @@
+# Day 1: Trebuchet?!
+
+https://adventofcode.com/2023/day/1
+
+```zsh
+% cargo run -p aocc-2023 --release
+```
--- src/2023/src/main.rs
+++ src/2023/src/main.rs
@@ -4,23 +4,56 @@ use std::path::Path;
 use std::process::ExitCode;
 
 static DATA_DIR: &str = "data";
+// NOTE: index matches its digit value
+static DIGITS: [&str; 10] = [
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+    "nine",
+];
 
 #[allow(dead_code)]
-impl AoC {
+impl AoCC {
     pub fn day1(input: &str) -> u32 {
-        input.lines().fold(0, |acc, dat| {
-            let left = dat.chars().find_map(|c| c.to_digit(10));
-            let right = dat.chars().rev().find_map(|c| c.to_digit(10));
+        input.lines().fold(0, |acc, line| {
+            let mut chars = line.chars();
+
+            // NOTE:
+            // * This doesn't work for "twenty" (20) or "thirteen" (13) etc.
+            let mut tmp = String::new();
+            let left = chars.find_map(|c| {
+                c.to_digit(10).or_else(|| {
+                    tmp.push(c);
+                    match DIGITS.iter().position(|d| tmp.contains(*d)) {
+                        Some(i) => Some(i as u32),
+                        _ => None,
+                    }
+                })
+            });
+
+            // NOTE:
+            // * eg. "seventeen" (17) can be treated as 7
+            // * eg. "foury" means 0
+            // * eg. "fourtyone" (41) can be treated as 1
+            tmp = String::new();
+            let right = chars.rev().find_map(|c| {
+                c.to_digit(10).or_else(|| {
+                    tmp.insert_str(0, &c.to_string());
+                    match DIGITS.iter().position(|d| tmp.contains(*d)) {
+                        Some(i) => Some(i as u32),
+                        _ => None,
+                    }
+                })
+            });
 
             acc + match (left, right) {
                 (Some(a), Some(b)) => a * 10 + b,
+                (Some(a), None) => a * 10 + a,
                 _ => unreachable!(),
             }
         })
     }
 }
 
-pub struct AoC;
+pub struct AoCC;
 
 #[allow(dead_code)]
 fn main() -> ExitCode {
@@ -33,7 +66,7 @@ fn main() -> ExitCode {
     let mut buf = String::new();
     f.read_to_string(&mut buf).expect("cannot read the file");
 
-    let result = AoC::day1(&buf);
+    let result = AoCC::day1(&buf);
     println!("{}", result);
 
     ExitCode::from(0)
@@ -46,7 +79,25 @@ mod test {
 
     #[test]
     fn test_day1() {
-        let result = AoC::day1("");
-        assert_eq!(123, result);
+        let result = AoCC::day1(
+            r#"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+"#,
+        );
+        assert_eq!(142, result);
+
+        let result = AoCC::day1(
+            r#"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"#,
+        );
+        assert_eq!(281, result);
     }
 }
