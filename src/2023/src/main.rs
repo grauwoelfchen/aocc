563e90c12953c55367483edf48a67fbbb70ca657
Yasha <yasuhiro.asaka@grauwoelfchen.net>
2023-12-18 05:17:09 +0900

    Add day2 solutions

--- src/2023/src/main.rs
+++ src/2023/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::env;
 use std::io::prelude::*;
@@ -5,14 +6,57 @@ use std::path::Path;
 use std::process::ExitCode;
 
 static DATA_DIR: &str = "data";
+
 // NOTE: index matches its digit value
 static DIGITS: [&str; 10] = [
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
     "nine",
 ];
 
+static CUBES: [(&str, u32); 3] = [("red", 12), ("green", 13), ("blue", 14)];
+
 #[allow(dead_code)]
 impl AoCC {
+    fn line_to_cube_map(line: &str) -> HashMap<&str, u32> {
+        let (_, games) = line.split_once(':').unwrap();
+        let cubes: Vec<&str> = games.split(|c| c == ';' || c == ',').collect();
+        let mut cubes_pair: Vec<(&str, u32)> = cubes
+            .iter()
+            .map(|s| {
+                // eg. " 3 red"
+                let mut exp = s.split(' ');
+                let num = (exp.nth(1).unwrap()).parse::<u32>().unwrap();
+                let key = exp.last().unwrap();
+                (key, num)
+            })
+            .collect();
+
+        // last values are used in HashMap
+        cubes_pair.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+        cubes_pair.iter().cloned().collect()
+    }
+
+    pub fn day2_part_one(input: &str) -> u32 {
+        let map: HashMap<&str, u32> = CUBES.iter().cloned().collect();
+
+        input.lines().enumerate().fold(0, |mut acc, (i, line)| {
+            let data = Self::line_to_cube_map(line);
+
+            if data.iter().all(|(k, v)| &map[k] >= v) {
+                acc += (i + 1) as u32;
+            }
+            acc
+        })
+    }
+
+    pub fn day2_part_two(input: &str) -> u32 {
+        input.lines().fold(0, |acc, line| {
+            let data = Self::line_to_cube_map(line);
+
+            acc + data.values().copied().reduce(|a, b| a * b).unwrap()
+        })
+    }
+
     pub fn day1(input: &str) -> u32 {
         input.lines().fold(0, |acc, line| {
             let mut i_c = line.char_indices();
@@ -78,9 +122,12 @@ fn main() -> ExitCode {
     let mut buf = String::new();
     f.read_to_string(&mut buf).expect("cannot read the file");
 
-    let result = AoCC::day1(&buf);
-    println!("{}", result);
-
+    let result = match n {
+        1 => AoCC::day1(&buf),
+        2 => AoCC::day2_part_two(&buf),
+        _ => unimplemented!(),
+    };
+    println!("day{}: {}", n, result);
     ExitCode::from(0)
 }
 
@@ -111,5 +158,31 @@ zoneight234
 "#,
         );
         assert_eq!(281, result);
+    }
+
+    #[test]
+    fn test_day2_part_one() {
+        let result = AoCC::day2_part_one(
+            r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"#,
+        );
+        assert_eq!(8, result);
+    }
+
+    #[test]
+    fn test_day2_part_two() {
+        let result = AoCC::day2_part_two(
+            r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"#,
+        );
+        assert_eq!(2286, result);
     }
 }
